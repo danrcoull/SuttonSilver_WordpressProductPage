198b99215342c8b01488ab502a6a4d77a458d9dd
Daniel Coull <d.coull@suttonsilver.co.uk>
2017-06-21 09:45:27 +0100

    var_dumps

--- Block/Frontend/Catalog/Product/View/Type/Bundle.php
+++ Block/Frontend/Catalog/Product/View/Type/Bundle.php
@@ -72,6 +72,6 @@ class Bundle extends \Magento\Bundle\Block\Catalog\Product\View\Type\Bundle
     {
         return $this->_fishpig->getFactory('Post')->create()->getCollection()
             ->addTermIdFilter($id,'tribe_events_cat')
-            ->addFieldToFilter('post_name',array('like','%Induction%'));
+            ->addFieldToFilter('name',array('like','%Induction%'));
     }
 }
