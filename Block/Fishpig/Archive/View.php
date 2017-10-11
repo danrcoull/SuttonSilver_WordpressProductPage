6a8a23a76e1e98afb88c9b902c93db4ddc59abc8
Your Name <you@example.com>
2017-10-11 16:25:17 +0100

    changes

--- Block/Fishpig/Archive/View.php
+++ Block/Fishpig/Archive/View.php
@@ -6,38 +6,19 @@
  * @author      Ben Tideswell <help@fishpig.co.uk>
  */
 
-namespace SuttonSilver\WordpressProductPages\Block\Fishig\Archive;
+namespace SuttonSilver\WordpressProductPage\Block\Fishig\Archive;
 
-use \FishPig\WordPress\Model\Archive;
 
 class View extends \FishPig\WordPress\Block\Archive\View
 {
-    protected  $_registry;
-
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \FishPig\WordPress\Block\Context $wpContext,
-        \Magento\Framework\Registry $registry,
-        array $data = []
-    )
-    {
-        parent::__construct($context, $data);
-        $this->_registry = $registry;
-
-    }
 
 	protected function _getPostCollection()
 	{
 
-        if($postTypeCode = $this->_registry->registry('wordpress_post_type')) {
-            $postTypeCode = $postTypeCode->getPostType();
-        }elseif($postTypeCode = $this->_registry->registry('wordpress_post')) {
-            $postTypeCode = $postTypeCode->getPostType();
-        }
-
+	    $postType = $_GET['post_type'];
 		return parent::_getPostCollection()
 			->addArchiveDateFilter($this->getArchiveId(), $this->getArchive()->getIsDaily())
-			->addPostTypeFilter($postTypeCode);
+			->addPostTypeFilter(($postType) ?: 'post');
 	}
 
 }
